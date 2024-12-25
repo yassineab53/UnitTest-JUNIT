b7de9803036a76671f57d94860dbcb6289621f5a
yassine4real <yssnabbou2002@gmail.com>
2024-12-25 16:15:11 +0100

    add test for ArithmeticException case to CalculateMethods class

--- src/test/java/CalculateMethodsTest.java
+++ src/test/java/CalculateMethodsTest.java
@@ -1,11 +1,23 @@
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class CalculateMethodsTest {
-    CalculateMethods cal = new CalculateMethods();
+    private CalculateMethods calculateMethods;
+
+    @BeforeEach
+    public void initTest(){
+        calculateMethods = new CalculateMethods();
+    }
     @Test
-    public void dev(){
-        assertEquals(2, cal.divide(10,5));
+    public void testDivide() {
+        assertEquals(10, calculateMethods.divide(100, 10));
+    }
+
+    @Test
+    public void testDivideByZero() {
+        assertThrows(ArithmeticException.class, () -> calculateMethods.divide(100, 0));
     }
 }
